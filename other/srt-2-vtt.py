d532807e21d08dd23cb5a4120ad094916d76eaf3
Luis <58019574+ludev84@users.noreply.github.com>
2025-05-14 22:01:15 -0600

    Convert srt subtitle to vtt

---
+++ other/srt-2-vtt.py
@@ -0,0 +1,7 @@
+from srt_to_vtt import srt_to_vtt
+
+path_to_my_srt_file = "./other/files/input.srt"
+path_to_converted_vtt_file = "./other/files/output.vtt"
+
+# converts example.srt into output.vtt
+srt_to_vtt(path_to_my_srt_file, path_to_converted_vtt_file)
